d40479e2ff111996d3e60fb9cf346778d04142e5
XXX <muhiddinovhusan0207@gmail.com>
2024-07-05 17:30:47 +0500

    x

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -50,7 +50,7 @@ const Technologies = () => {
     return (
         <div className='border-b border-neutral-800 pb-24'>
             <h1 className='my-20 text-center text-4xl'>Technologies</h1>
-            <div className='flex flex-wrap items-center overflow-x-scroll justify-center gap-4'>
+            <div className='flex flex-wrap items-center justify-center gap-4'>
                 <AnimatePresence>
                     {visibleTechnologies.map((tech, index) => (
                         <motion.div
